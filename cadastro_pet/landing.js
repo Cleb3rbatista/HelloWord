fa6d049a04435eab6f39d7b828f1ad3130c9354a
Cleb3rbatista <101151617+Cleb3rbatista@users.noreply.github.com>
2022-05-30 22:22:52 -0300

    criei js

---
+++ cadastro_pet/landing.js
@@ -0,0 +1,19 @@
+function vira_azul()
+{
+    let Bot= document.getElementById("cor");
+    Bot.style.backgroundColor="blue";
+}
+function vira_amarelo()
+{
+    let Bot= document.getElementById("cor");
+    Bot.style.backgroundColor="yellow";
+}
+function vira_vermelho()
+{
+    let bot2= document.getElementById("cor");
+    bot2.style.backgroundColor="red";
+}
+function enviado()
+{
+    alert("cadastro efetuado");
+}
